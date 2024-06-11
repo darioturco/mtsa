28d1b7f6553d5ea3f2f114824ec904c36e4185a8
Dario <diort.2018@gmail.com>
2024-06-11 19:55:19 -0300

    main updated

--- MTSApy/main.py
+++ MTSApy/main.py
@@ -2,10 +2,12 @@ from src.experiments import TestTrainedInAllInstances, TrainSmallInstance
 
 if __name__ == "__main__":
     experiment_name = "CRL"
-    instance = "CM"
+    instance = "TA"
 
-    TrainSmallInstance().train(instance, 2, 2, experiment_name)
-    TestTrainedInAllInstances().select_with_java(instance, experiment_name, 1000)
-    TestTrainedInAllInstances().test_with_java(instance, experiment_name, 15000, None)
+    #TrainSmallInstance().train(instance, 2, 2, experiment_name)
+    #TestTrainedInAllInstances().select_with_java(instance, experiment_name, 1000)
+    #TestTrainedInAllInstances().test_with_java(instance, experiment_name, 15000, None)
+
+    TestTrainedInAllInstances().test_with_java(instance, experiment_name, 15000, "./results/models/TA/CRL-TA.onnx")
 
 
