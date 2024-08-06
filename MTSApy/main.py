f5d93c270b395cd8d14a9757a05d9b88fc0e1e17
fzanollo <fzanollo@dc.uba.ar>
2024-08-06 15:25:23 -0300

    run all

--- MTSApy/main.py
+++ MTSApy/main.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     experiment_name = "ROLES"
     instance = "DP"
 
-    # TrainSmallInstance().train(instance, 2, 2, experiment_name)
-    # TestTrainedInAllInstances().select_with_java(instance, experiment_name, 1000)
+    TrainSmallInstance().train(instance, 2, 2, experiment_name)
+    TestTrainedInAllInstances().select_with_java(instance, experiment_name, 1000)
     TestTrainedInAllInstances().test_with_java(instance, experiment_name, 15000, None)
 
     # onyx_path = "MTSApy/results/models/BW/CRL-BW-1.onnx"
